a3b4f657de2af9f0a3ce69ecedaa499c44a6604b
Jonathan Lane <jon@borg.moe>
2025-12-09 10:29:39 -0800

    don't overwrite envvars for gnome session

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,7 +19,6 @@ dnf5 install -y tmux mosh emacs git-email git-lfs postfix ckermit maildir-utils 
 #
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 #dnf5 -y copr enable errornointernet/quickshell
 #dnf5 -y copr enable rockowitz/ddcutil
@@ -35,6 +34,8 @@ dnf5 install -y tmux mosh emacs git-email git-lfs postfix ckermit maildir-utils 
 #dnf5 -y copr enable avengemedia/danklinux
 #dnf5 -y copr enable avengemedia/dms
 #dnf5 -y install dms qt6-qtmultimedia cliphist ghostty hyprpicker matugen
+
+# Disable COPRs so they don't end up enabled on the final image:
 
 #### Example for enabling a System Unit File
 
